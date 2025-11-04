3a063d0900df70bd2e7f2e84a723408498237db7
DullNotDev <sgiyonosgiyono@gmail.com>
2025-11-04 11:03:53 +0700

    Delete docs/database directory

--- docs/database/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  tokens: "8579534295:AAENIQX9gcOcHIKZVdp_Q9jIPgSskxyEX_k",  // Ubah Jadi Token Bot Mu !!!
-  owner: "5826556772", // Ubah Jadi Id Mu !!!
-  port: "2600", // Ubah Jadi Port Panel Mu !!!
-  ipvps: "24.144.88.134" // Ubah Jadi Ip Vps Mu !!!
-};
